43a50bc6e40763c0d3ea3e311e516dce3c22b73d
salva <salva@LAPTOP-B2KFIIUB>
2021-03-02 16:30:11 -0600

    Tipos de datos area_66136.

---
+++ src/tipodedatosarea66136/TipoDeDatosArea66136.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tipodedatosarea66136;
+
+/**
+ *
+ * @author salva
+ */
+public class TipoDeDatosArea66136 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+      
+     //Para obtener el area de un rectangulo
+     // es necesario esta formula a= base*altura 
+     
+     int area_del_rectangulo = 0;
+     int base = 55;
+     int altura = 80;
+     
+     area_del_rectangulo = base*altura;
+        
+      System.out.println("El area de un rectangulo de base "+base+" y altura "+altura+" es igual a "+area_del_rectangulo);
+     //de esta forma obtenemos el valor del area de un rectangulo 
+        
+        
+    }
+    
+}
